2a7c0f79768d6224613331d9cd9febc057f46e7c
Ivane$$ence <ivan.zelezinski@gmail.com>
2025-10-30 16:41:52 +0300

    new: add Solution 44 (https://leetcode.com/problems/wildcard-matching/description/)

--- src/main/java/by/ivan/zelezinski/test/project/Main.java
+++ src/main/java/by/ivan/zelezinski/test/project/Main.java
@@ -2,12 +2,14 @@ package by.ivan.zelezinski.test.project;
 
 
 import by.ivan.zelezinski.test.project.task43.Solution43;
+import by.ivan.zelezinski.test.project.task44.Solution44;
 
 public class Main {
     public static void main(String[] args) {
-        String a = "987654321987654321";
-        String b = "123456789123456789";
-        Solution43 solution43 = new Solution43();
-        System.out.println(solution43.multiply(a , b));
+
+        Solution44 solution44 = new Solution44();
+        String s ="a";
+        String p ="*a*";
+        System.out.println(solution44.isMatch(s, p));
     }
 }
