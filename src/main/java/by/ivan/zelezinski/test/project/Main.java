4b648c2d7063f0c6ed5d163142889e45887a5294
Ivane$$ence <ivan.zelezinski@gmail.com>
2026-04-21 00:29:33 +0300

    new: add Solution 202 https://leetcode.com/problems/happy-number/description/

--- src/main/java/by/ivan/zelezinski/test/project/Main.java
+++ src/main/java/by/ivan/zelezinski/test/project/Main.java
@@ -1,11 +1,11 @@
 package by.ivan.zelezinski.test.project;
 
 
-import by.ivan.zelezinski.test.project.task191.Solution191;
+import by.ivan.zelezinski.test.project.task202.Solution202;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(new Solution191().hammingWeight(128));
+        System.out.println(new Solution202().isHappy(2));
     }
 }
